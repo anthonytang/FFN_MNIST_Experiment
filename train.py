df2117e293d4d476de4787a3a91ad667e020d1e4
Anthony Tang <anthonytang@Anthonys-MacBook-Pro-3.local>
2025-07-31 16:43:00 -0500

    run experiment code

--- train.py
+++ train.py
@@ -62,3 +62,62 @@ class LitModel(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+def bootstrap(data, k=1000):
+    return np.std([np.mean(np.random.choice(data, len(data))) for _ in range(k)])
+
+def loader(s, b, shuffle=False):
+    return DataLoader(s, batch_size=b, shuffle=shuffle, num_workers=4)
+
+def run(hidden_dims=[2, 4, 8, 16], ks=[2, 4, 8]):
+    data = datasets.MNIST('', train=True, download=True, transform=transforms.ToTensor())
+    test_data = datasets.MNIST('', train=False, download=True, transform=transforms.ToTensor())
+    all_results = {"FFN_ReLU": {}, "FFN_GeGLU": {}}
+
+    for Model in [FFN_ReLU, FFN_GeGLU]:
+        name = Model.__name__
+        for k in ks:
+            accs = []
+            for d in hidden_dims:
+                best_acc, best_model = 0, None
+                for _ in range(k):
+                    bs = random.choice([8, 64])
+                    lr = random.choice([1e-1, 1e-2, 1e-3, 1e-4])
+                    train_set, val_set = random_split(data, [50000, 10000])
+                    m = Model(28 * 28, d, 10)
+                    lit = LitModel(m, lr)
+                    trainer = pl.Trainer(max_epochs=1, logger=False, enable_checkpointing=False, enable_progress_bar=False)
+                    trainer.fit(lit, loader(train_set, bs, shuffle=True), loader(val_set, bs))
+                    val_result = trainer.validate(lit, loader(val_set, bs), verbose=False)
+                    val_acc = val_result[0].get('val_acc', 0.0) if val_result else 0.0
+                    if val_acc > best_acc:
+                        best_acc, best_model = val_acc, lit
+                test_accs = [x['test_acc'] for x in pl.Trainer(logger=False).test(best_model, DataLoader(test_data, batch_size=64))]
+                accs.append((test_accs[0], bootstrap(test_accs)))
+            all_results[name][k] = accs
+
+    for k in ks:
+        plt.figure()
+        for model in all_results:
+            means = [all_results[model][k][i][0] for i in range(len(hidden_dims))]
+            errs = [all_results[model][k][i][1] for i in range(len(hidden_dims))]
+            plt.errorbar(hidden_dims, means, yerr=errs, label=model, marker='o', capsize=5)
+        plt.title(f"MNIST Test Accuracy vs Hidden Dim (k={k})")
+        plt.xlabel("Hidden Dim")
+        plt.ylabel("Test Accuracy")
+        plt.ylim(0.5, 1.0)
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(f"k{k}.png")
+        plt.close()
+
+        relu_best = max([acc for acc, _ in all_results["FFN_ReLU"][k]])
+        geglu_best = max([acc for acc, _ in all_results["FFN_GeGLU"][k]])
+        if geglu_best > relu_best:
+            print(f"[k={k}] GeGLU outperformed ReLU: {geglu_best:.4f} vs {relu_best:.4f}")
+        else:
+            print(f"[k={k}] ReLU outperformed or matched GeGLU: {relu_best:.4f} vs {geglu_best:.4f}")
+
+if __name__ == "__main__":
+    run()
